dd8d3cced3c6ebc546c1510b2c3357287234ec1f
T_Takebayashi <chikurin1014@gmail.com>
2023-08-02 18:11:58 +0900

    minimalモードのインストールからrust関連を削除
    Changes to be committed:
    	modified:   installation/_exec_minimal.sh

--- installation/_exec_minimal.sh
+++ installation/_exec_minimal.sh
@@ -9,24 +9,10 @@ INSTALL_BY_APT=(
     "libssl-dev"
 )
 
-INSTALL_BY_CARGO=(
-    "cargo-update"
-    "cargo-generate"
-)
-
 function install_apt() {
     ARG=$1
     if [ -z "`apt list --installed | grep "$ARG"`" ]; then
         echo "$SUDO_PWD" | sudo -S apt-get install -yq $ARG
-    else
-        echo "    already exists"
-    fi
-}
-
-function install_cargo() {
-    ARG=$1
-    if [ -z "`cargo install --list | grep "$ARG"`" ]; then
-        cargo install -q --message-format human "$ARG"
     else
         echo "    already exists"
     fi
@@ -41,25 +27,8 @@ echo "$pac"
 install_apt "$pac"
 done
 
-# install rustup which includes cargo
-# ref: https://www.rust-lang.org/ja/learn/get-started
-echo "rustup"
-if [ ! -e `which rustup` ]; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-else
-    echo "    already exists"
-fi
-
-# install packages by cargo
-for pac in ${INSTALL_BY_CARGO[@]}
-do
-echo "$pac"
-install_cargo "$pac"
-done
-
 # update & upgrade packages
 echo "Updating & Upgrading packages"
 
 echo "$SUDO_PWD" | sudo -S apt-get -y update
 echo "$SUDO_PWD" | sudo -S apt-get -y upgrade
-echo "$SUDO_PWD" | sudo -S cargo install-update --all
