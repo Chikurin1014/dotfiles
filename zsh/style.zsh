2d118cb4e96fad1a4c82294f3e9c669ef15ba322
T_Takebayashi <chikurin1014@gmail.com>
2023-08-02 17:21:45 +0900

    プロンプトを編集
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   zsh/style.zsh

--- zsh/style.zsh
+++ zsh/style.zsh
@@ -10,7 +10,7 @@ line_head="%# "
 colored_host_at_user="%F{64}[%f%F{83}${user}%f%F{64}@%f%F{83}${host}%f%F{64}]%f"
 colored_dir="%F{202}${dir}%f"
 colored_line_head="%F{172}${line_head}%f"
-colored_ymdt="%F{202}${date}%f%F{202}${T}%f"
+colored_ymdt="%F{202}${T} - ${date}%f"
 
 PROMPT="${colored_host_at_user} ${colored_dir}"$'\n'"${colored_line_head}"
 RPROMPT="${colored_ymdt}"
