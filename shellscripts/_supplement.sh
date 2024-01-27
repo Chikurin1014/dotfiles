cf8e4f2d07f21dcca73034975a610a021bb0f2f3
Tokoroten2003 <chikurin1014@gmail.com>
2024-01-27 22:24:35 +0900

    [fix] not to represent error if the link exists

--- shellscripts/_supplement.sh
+++ shellscripts/_supplement.sh
@@ -2,4 +2,10 @@
 
 mkdir -p ~/.local/bin
 
-ln -s /usr/bin/batcat ~/.local/bin/bat
+which batcat > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+    which bat > /dev/null 2>&1
+    if [[ $? -eq 1 ]]; then
+        ln -s /usr/bin/batcat ~/.local/bin/bat
+    fi
+fi
