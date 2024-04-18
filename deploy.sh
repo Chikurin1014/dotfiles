df552308991274bf401c2de02d2fbc07ed562224
Chikurin1014 <chikurin1014@gmail.com>
2024-04-18 13:06:21 +0900

    remove dry-run option

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ source "$SRC_DIR/shellscripts/definition.sh" $@
 source "$SRC_DIR/shellscripts/update_apt.sh" $@
 source "$SRC_DIR/shellscripts/install.sh" $@
 echo "Linking files"
-source "$SRC_DIR/sl-maker-for-dotfiles/run.sh" -o "$SRC_DIR/files" -d "$HOME" -n > "$SRC_DIR/sl-maker.log"
+source "$SRC_DIR/sl-maker-for-dotfiles/run.sh" -o "$SRC_DIR/files" -d "$HOME" > "$SRC_DIR/sl-maker.log"
 source "$SRC_DIR/shellscripts/update_apt.sh" $@
 echo "Done"
 echo ""
