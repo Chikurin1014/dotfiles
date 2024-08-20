4b4517be1d2ce9a543265ddf37448ae053b4f99a
Chikurin1014 <chikurin1014@gmail.com>
2024-08-21 00:44:29 +0900

    remove tmux conf

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,12 @@ source "$SRC_DIR/shellscripts/definition.sh" "$@"
 source "$SRC_DIR/shellscripts/update_apt.sh" "$@"
 source "$SRC_DIR/shellscripts/install.sh" "$@"
 echo "Linking files"
-source "$SRC_DIR/sl-maker-for-dotfiles/run.sh" -o "$SRC_DIR/files" -d "$HOME" -r "zshrc -> .zshrc" -r "tmux.conf -> .tmux.conf" -r "config/ -> .config/" > "$SRC_DIR/sl-maker.log"
+source "$SRC_DIR/sl-maker-for-dotfiles/run.sh" \
+    -o "$SRC_DIR/files" \
+    -d "$HOME" \
+    -r "zshrc -> .zshrc" \
+    -r "config/ -> .config/" \
+    > "$SRC_DIR/sl-maker.log"
 source "$SRC_DIR/shellscripts/update_apt.sh" "$@"
 echo "Done"
 echo ""
