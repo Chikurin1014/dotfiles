e02df57b6928cc4ba11437bad3d3d50d113c881f
Chikurin1014 <chikurin1014@gmail.com>
2025-08-09 00:07:39 +0900

    :zap: improve wezterm window

--- files_win/config/wezterm/wezterm.lua
+++ files_win/config/wezterm/wezterm.lua
@@ -19,20 +19,26 @@ end
 
 config.front_end = "WebGpu"
 config.default_prog = { 'nu', '-l' }
-config.initial_rows = 25
-config.initial_cols = 100
-config.window_decorations = 'RESIZE'
-config.inactive_pane_hsb = { hue = 1.0, saturation = 0.8, brightness = 0.8 }
+config.initial_rows = 24
+config.initial_cols = 80
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+config.inactive_pane_hsb = { hue = 1.0, saturation = 1.0, brightness = 0.9 }
+config.hide_tab_bar_if_only_one_tab = false
 
 config.font = font.property
-config.font_size = 10.5
+config.font_size = font.size
 config.color_scheme = theme.color_scheme
+config.colors = theme.colors
 config.use_fancy_tab_bar = theme.use_fancy_tab_bar
+config.tab_bar_at_bottom = theme.tab_bar_at_bottom
 config.window_frame = theme.window_frame
 config.background = theme.background
+config.show_new_tab_button_in_tab_bar = theme.show_new_tab_button_in_tab_bar
 config.disable_default_key_bindings = true
 config.leader = keybindings.leader
 config.keys = keybindings.keys
+
+wezterm.on('format-tab-title', theme.format_tab_title)
 
 -- and finally, return the configuration to wezterm
 return config
