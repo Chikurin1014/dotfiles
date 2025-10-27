b12eed3cc330054e7d6ed3f3ed4eb5b31b2597dc
Chikurin1014 <chikurin1014@gmail.com>
2025-10-27 11:05:51 +0900

    :sparkles: lazygitでもgitmojiを使えるようにした

--- files/config/nvim/lua/plugins/comment.lua
+++ files/config/nvim/lua/plugins/comment.lua
@@ -11,14 +11,14 @@ return {
             -- -LHS of toggle mappings in NORMAL mode
             toggler = {
                 ---Line-comment toggle keymap
-                line = '<C-/>',
+                line = '<leader>/'
                 ---Block-comment toggle keymap
                 -- block = '<C-b>',
             },
             ---LHS of operator-pending mappings in NORMAL and VISUAL mode
             opleader = {
                 ---Line-comment keymap
-                line = '<C-/>',
+                line = '<leader>/',
                 ---Block-comment keymap
                 -- block = '<C-b>',
             },
