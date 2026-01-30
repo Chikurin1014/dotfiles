f6e6d27e5eff518abbcf94cd378cb69d03f0b80b
Chikurin1014 <chikurin1014@gmail.com>
2026-01-30 15:29:47 +0900

    :sparkles: configure dasboard

--- files/config/nvim/lua/plugins/telescope.lua
+++ files/config/nvim/lua/plugins/telescope.lua
@@ -1,18 +1,20 @@
 return {
      'nvim-telescope/telescope.nvim',
      keys = {
-         { '<leader>ff', mode = 'n', desc = 'Find files' },
-         { '<leader>f/', mode = 'n', desc = 'Find words (live grep)' },
-         { '<leader>fb', mode = 'n', desc = 'Find buffers' },
-         { '<leader>fh', mode = 'n', desc = 'Find helps' },
-         { '<leader>ft', mode = 'n', desc = 'Find functions, variables, etc.' },
-         { '<leader>fn', mode = 'n', desc = 'Find notifications' }
+         { '<leader>ff', mode = 'n', desc = 'Find a file' },
+         { '<leader>fr', mode = 'n', desc = 'Find a recent file' },
+         { '<leader>f/', mode = 'n', desc = 'Find a word (live grep)' },
+         { '<leader>fb', mode = 'n', desc = 'Find a buffer' },
+         { '<leader>fh', mode = 'n', desc = 'Find a help' },
+         { '<leader>ft', mode = 'n', desc = 'Find a function, variable, etc.' },
+         { '<leader>fn', mode = 'n', desc = 'Find a notification' }
      },
      dependencies = { 'nvim-lua/plenary.nvim' },
      config = function()
         local telescope = require('telescope')
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
         vim.keymap.set('n', '<leader>f/', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
--- files/config/nvim/lua/plugins/ui/alpha.lua
+++ files/config/nvim/lua/plugins/ui/alpha.lua
@@ -4,7 +4,34 @@ return {
         event = 'VimEnter',
         dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function ()
-            require'alpha'.setup(require'alpha.themes.dashboard'.config)
+            local alpha = require('alpha')
+            local dashboard = require('alpha.themes.dashboard')
+
+            local header_art_path = vim.fs.joinpath(vim.fn.stdpath('config'), 'dashboard.txt')
+            if vim.fn.filereadable(header_art_path) == 1 then
+                dashboard.section.header.val = vim.fn.readfile(header_art_path)
+            end
+
+            dashboard.section.buttons.val = {
+                dashboard.button('n', 'New file', '<cmd>ene<BAR>startinsert<cr>'),
+                dashboard.button('o', 'Open a file', '<cmd>lua require("telescope.builtin").find_files()<cr><esc>'),
+                dashboard.button('r', 'Open a recent file', '<cmd>lua require("telescope.builtin").oldfiles()<cr><esc>'),
+                dashboard.button('g', 'Git source control', '<cmd>LazyGit<cr>'),
+                dashboard.button('p', 'Manage plugins', '<cmd>Lazy<cr>'),
+                dashboard.button('q', 'Quit', '<cmd>qa<cr>')
+            }
+
+            local footer = function ()
+                local datetime = os.date('%Y/%m/%d - %H:%M:%S')
+                local total_plugins = require('lazy').stats().count
+                local version = vim.version()
+                local nvim_version_info = string.format('v%d.%d.%d', version.major, version.minor, version.patch)
+
+                return string.format('%s    %d plugins    Neovim: %s', datetime, total_plugins, nvim_version_info)
+            end
+            dashboard.section.footer.val = footer()
+
+            alpha.setup(dashboard.config)
         end
     }
 }
