d901808140d825d7f25a2b74e6936335b25fa225
Chikurin1014 <chikurin1014@gmail.com>
2026-02-08 02:20:07 +0900

    :adhesive_bandage: modify gitsigns.nvim config

--- files/config/nvim/lua/plugins/gitsigns.lua
+++ files/config/nvim/lua/plugins/gitsigns.lua
@@ -5,6 +5,15 @@ return {
 		event = 'BufReadPre',
 		cmd = 'Gitsigns',
 		opts = {
+			signs_staged_enable = true,
+			signcolumn = false,
+			numhl = true,
+			linehl = false,
+			word_diff = false,
+			watch_gitdir = {
+				follow_files = true,
+			},
+			current_line_blame = true,
 			on_attach = function(bufnr)
 				local gitsigns = require('gitsigns')
 				local map = function(mode, l, r, opts)
@@ -27,31 +36,30 @@ return {
 					gitsigns.nav_hunk('prev')
 				end)
 
-				map('n', '<leader>gs', gitsigns.stage_hunk, { desc = 'Stage hunk' })
-				map('n', '<leader>gu', gitsigns.undo_stage_hunk, { desc = 'Undo stage hunk' })
+				map('n', '<leader>gs', gitsigns.stage_hunk, { desc = 'Stage/Unstage hunk' })
 				map('n', '<leader>gr', gitsigns.reset_hunk, { desc = 'Reset hunk' })
 				map('v', '<leader>gs', function()
 					gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-				end, { desc = 'Stage hunk' })
+				end, { desc = 'Stage/Unstage hunk' })
 				map('v', '<leader>gr', function()
 					gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
-				end, { desc = 'Reset hunk' })
-				map('n', '<leader>gp', gitsigns.preview_hunk, { desc = 'Preview hunk' })
+				end, { desc = 'Reset/Unstage hunk' })
 				map('n', '<leader>gi', gitsigns.preview_hunk_inline, { desc = 'Preview hunk inline' })
 
-				map('n', '<leader>gS', gitsigns.stage_buffer, { desc = 'Stage buffer' })
+				map('n', '<leader>gS', gitsigns.stage_buffer, { desc = 'Stage/Unstage buffer' })
 				map('n', '<leader>gR', gitsigns.reset_buffer, { desc = 'Reset buffer' })
-				
+
 				map('n', '<leader>gb', function() gitsigns.blame_line({ full = true }) end, { desc = 'Blame line' })
-				
+
 				map('n', '<leader>gd', gitsigns.diffthis, { desc = 'Git diff' })
-				map('n', '<leader>gD', function() gitsigns.diffthis('~') end, { desc = 'Git diff against HEAD' })
+				map('n', '<leader>gD', function() gitsigns.diffthis('~') end,
+					{ desc = 'Git diff against the last commit' })
 
-				map('n', '<leader>gq', gitsigns.setqflist, { desc = 'Set qflist' })
-				map('n', '<leader>gQ', function() gitsigns.setqflist('all') end, { desc = 'Set qflist (all)' })
-
-				map('n', '<leader>tgb', gitsigns.toggle_current_line_blame, { desc = 'Toggle current line blame' })
+				map('n', '<leader>tgs', gitsigns.toggle_signs, { desc = 'Toggle signs' })
+				map('n', '<leader>tgn', gitsigns.toggle_numhl, { desc = 'Toggle number highlight' })
+				map('n', '<leader>tgl', gitsigns.toggle_linehl, { desc = 'Toggle line highlight' })
 				map('n', '<leader>tgw', gitsigns.toggle_word_diff, { desc = 'Toggle word diff' })
+				map('n', '<leader>tgb', gitsigns.toggle_current_line_blame, { desc = 'Toggle current line blame' })
 
 				map({ 'o', 'x' }, 'ig', gitsigns.select_hunk, { desc = 'git hunk' })
 			end
