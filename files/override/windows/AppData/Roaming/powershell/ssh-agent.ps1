d3db541a5be0881a7a063654b6d09e49d0bf059c
Chikurin1014 <chikurin1014@gmail.com>
2026-02-09 22:51:06 +0900

    :adhesive_bandage: disble ssh-agent startup

--- files/override/windows/AppData/Roaming/powershell/ssh-agent.ps1
+++ files/override/windows/AppData/Roaming/powershell/ssh-agent.ps1
@@ -1,5 +1,7 @@
-$SSHAgentService = Get-Service -Name 'ssh-agent' -ErrorAction SilentlyContinue
-if ($null -ne $SSHAgentService -and $SSHAgentService.Status -ne 'Running') {
-    # $SSHAgentService | Set-Service -StartupType Automatic
-    Start-Service 'ssh-agent'
-}
+# $SSHAgentService = Get-Service -Name 'ssh-agent' -ErrorAction SilentlyContinue
+# if ($null -ne $SSHAgentService -and $SSHAgentService.Status -ne 'Running') {
+#     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+#     # $SSHAgentService | Set-Service -StartupType Automatic
+#     $SSHAgentService | Start-Service 
+#     Set-ExecutionPolicy -ExecutionPolicy Undefined -Scope Process
+# }
