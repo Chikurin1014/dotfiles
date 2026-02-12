8e61f7bb457c0f2ff46b208f013bda016a110087
Chikurin1014 <chikurin1014@gmail.com>
2026-02-12 15:13:35 +0000

    :recycle: refactor deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 
-if which home-manager > /dev/null 2>&1; then
-    home-manager switch --flake ~/dotfiles -b bak
+SRC_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
+
+if command -v home-manager > /dev/null 2>&1; then
+    home-manager switch --flake $SRC_DIR -b bak
     exit $?
 fi
 
-if which nix > /dev/null 2>&1; then
-    nix --extra-experimental-features nix-commands,flakes run home-manager switch -- --flake ~/dotfiles -b bak
+if command -v nix > /dev/null 2>&1; then
+    nix --extra-experimental-features nix-commands,flakes run home-manager switch -- --flake $SRC_DIR -b bak
     exit $?
 fi
 
-if which dotter > /dev/null 2>&1; then
+if command -v dotter > /dev/null 2>&1; then
+    cd $SRC_DIR
     dotter deploy --force
     exit $?
 fi
 
-SRC_DIR=$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)
+if [ ! -x "$SRC_DIR/sl-maker-for-dotfiles/run.sh" ]; then
+    echo "An executable file \`sl-maker-for-dotfiles/run.sh\` is not found" >&2
+    echo "Be sure to initialize & update git submodule by:" >&2
+    echo "\`git submodule init && git submodule update\`" >&2
+    exit 1
+fi
 
 source "$SRC_DIR/sl-maker-for-dotfiles/run.sh" \
     -o "$SRC_DIR/files" \
