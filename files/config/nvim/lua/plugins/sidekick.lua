ef22b58d42aca01e8ebb1a96623c0725ec7f68f7
Chikurin1014 <chikurin1014@gmail.com>
2026-02-18 10:42:25 +0900

    :fire: remove unnecessary keybinds

--- files/config/nvim/lua/plugins/sidekick.lua
+++ files/config/nvim/lua/plugins/sidekick.lua
@@ -3,60 +3,6 @@ return {
         "folke/sidekick.nvim",
         lazy = true,
         cmd = 'Sidekick',
-        keys = {
-            {
-                "<Tab>",
-                function()
-                    -- if there is a next edit, jump to it, otherwise apply it if any
-                    if not require("sidekick").nes_jump_or_apply() then
-                        return "<Tab>" -- fallback to normal tab
-                    end
-                end,
-                expr = true,
-                desc = "Goto/Apply Next Edit Suggestion",
-            },
-            {
-                "<leader>ta",
-                function() require("sidekick.cli").toggle() end,
-                desc = "Toggle Sidekick Agent",
-                mode = { "n", "t", "i", "x" },
-            },
-            {
-                "<leader>as",
-                function() require("sidekick.cli").select() end,
-                -- Or to select only installed tools:
-                -- require("sidekick.cli").select({ filter = { installed = true } })
-                desc = "Select CLI",
-            },
-            {
-                "<leader>ad",
-                function() require("sidekick.cli").close() end,
-                desc = "Detach a CLI Session",
-            },
-            {
-                "<leader>at",
-                function() require("sidekick.cli").send({ msg = "{this}" }) end,
-                mode = { "x", "n" },
-                desc = "Send This",
-            },
-            {
-                "<leader>af",
-                function() require("sidekick.cli").send({ msg = "{file}" }) end,
-                desc = "Send File",
-            },
-            {
-                "<leader>av",
-                function() require("sidekick.cli").send({ msg = "{selection}" }) end,
-                mode = { "x" },
-                desc = "Send Visual Selection",
-            },
-            {
-                "<leader>ap",
-                function() require("sidekick.cli").prompt() end,
-                mode = { "n", "x" },
-                desc = "Sidekick Select Prompt",
-            },
-        },
         opts = {
             cli = {
                 mux = { enabled = vim.g.is_unix },
