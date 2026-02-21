93e44865bb1e5f9a8dd5b65e1039b811832a7838
Chikurin1014 <chikurin1014@gmail.com>
2026-02-22 00:23:11 +0900

    :recycle: modify lualine configuration

--- files/config/nvim/lua/plugins/ui/lualine.lua
+++ files/config/nvim/lua/plugins/ui/lualine.lua
@@ -6,31 +6,43 @@ return {
             { 'nvim-mini/mini.icons',         lazy = true },
             { 'meuter/lualine-so-fancy.nvim', lazy = true },
         },
-        event = 'BufEnter',
+        event = 'UIEnter',
         opts = {
             options = {
                 theme = 'iceberg',
                 component_separators = '',
                 section_separators = { left = '', right = '' },
-                background = 'dark',
                 disabled_filetypes = {
-                    statusline = { 'dashboard' },
+                    statusline = { 'alpha', 'dashboard', 'snacks_dashboard' },
                 },
                 always_show_tabline = false,
                 globalstatus = true,
             },
             sections = {
-                lualine_a = { 'fancy_mode' },
-                lualine_b = { { 'fancy_branch' }, { 'fancy_diff' }, { 'fancy_diagnostics' } },
-                lualine_c = { { 'fancy_location' }, { 'fancy_cwd', substitute_home = true } },
-                lualine_x = { { 'fancy_macro' }, { 'encoding' }, { 'fileformat' }, { 'fancy_filetype' } },
+                lualine_a = {
+                    {
+                        'mode',
+                        fmt = function(text)
+                            local icons = {
+                                ['NORMAL'] = '',
+                                ['VISUAL'] = '',
+                                ['INSERT'] = '󰗧',
+                                ['TERMINAL'] = '',
+                            }
+                            return icons[text]
+                                and icons[text]
+                                or text:sub(1, 1)
+                        end,
+                    },
+                },
+                lualine_b = { { 'branch' }, { 'fancy_diff' }, { 'fancy_diagnostics' } },
+                lualine_c = { { 'location', icon = ' ' }, { 'fancy_cwd', substitute_home = true } },
+                lualine_x = { { 'fancy_macro' }, { 'encoding', padding = 0 }, { 'fileformat' }, { 'fancy_filetype', ts_icon = ' ' } },
                 lualine_y = { { 'fancy_lsp_servers' } },
-                lualine_z = { { '"  " .. os.date("%H:%M")' } },
+                lualine_z = { { 'datetime', style = '%H:%M', icon = ' ' } },
             },
             tabline = { lualine_z = { { 'tabs' } } },
-            winbar = {},
-            inactive_winbar = {},
-            extensions = {}
+            extensions = { 'mason', 'lazy' }
         },
         config = function(_, opts)
             require('mini.icons').mock_nvim_web_devicons()
