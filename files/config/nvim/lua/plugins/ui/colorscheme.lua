fd04770badd2310108a7f423d486fd6a4822d170
Chikurin1014 <chikurin1014@gmail.com>
2026-02-22 09:58:55 +0900

    :recycle: refactor colorscheme settings

--- files/config/nvim/lua/plugins/ui/colorscheme.lua
+++ files/config/nvim/lua/plugins/ui/colorscheme.lua
@@ -1,26 +1,30 @@
-local function import_theme(plugin, mod, theme)
-    local th = theme and theme or mod
-    local ok, specs = pcall(require, 'plugins.ui.themes.' .. th)
+local function import_theme(plugin, mod, options)
+    if not options then options = {} end
+    local theme = options.theme and options.theme or mod
+    local ok, specs = pcall(require, 'plugins.ui.themes.' .. theme)
     if not ok then
         specs = {}
     end
-    local enabled = th == vim.g.default_colorscheme
-    local opts = specs.opts and specs.opts or {}
+    local enabled = options.enabled and options.enabled
+        or specs.enabled and specs.enabled
+        or theme == vim.g.default_colorscheme
     local config = specs.config and specs.config or function(_, opts)
         require(specs.main and specs.main or mod).setup(opts)
-        vim.cmd.colorscheme(th)
+        if theme == vim.g.default_colorscheme then
+            vim.cmd.colorscheme(theme)
+        end
     end
     return {
         plugin,
         enabled = enabled,
         lazy = true,
         event = 'UIEnter',
-        opts = opts,
+        opts = specs.opts and specs.opts or {},
         config = config,
     }
 end
 
 return {
-    import_theme('rebelot/kanagawa.nvim', 'kanagawa'),
-    import_theme('EdenEast/nightfox.nvim', 'nightfox'),
+    import_theme('rebelot/kanagawa.nvim', 'kanagawa', { enabled = true }),
+    import_theme('EdenEast/nightfox.nvim', 'nightfox', { enabled = true }),
 }
