a429ddb0189da11ae90ca7b924bf8dab38e4cf94
Chikurin1014 <chikurin1014@gmail.com>
2026-02-23 16:14:30 +0000

    :sparkles: configure completion

--- files/config/nvim/lua/plugins/blink-cmp.lua
+++ files/config/nvim/lua/plugins/blink-cmp.lua
@@ -2,12 +2,18 @@ return {
     {
         'saghen/blink.cmp',
         version = '1.*',
-        dependencies = {
-            -- optional: provides snippets for the snippet source
-            'rafamadriz/friendly-snippets',
-        },
         specs = {
+            { 'nvim-mini/mini.icons',      lazy = true },
             { 'xzbdmw/colorful-menu.nvim', lazy = true },
+            -- wrapper of sources for nvim-cmp
+            {
+                'saghen/blink.compat', -- use v2.* for blink.cmp v1.*
+                version = '2.*',
+                lazy = true
+            },
+            -- sources
+            { 'rafamadriz/friendly-snippets', lazy = true }, -- snippets
+            { 'Kaiser-Yang/blink-cmp-git',    lazy = true }, -- git
         },
         lazy = true,
         event = { 'InsertEnter', 'CmdLineEnter' },
@@ -21,6 +27,19 @@ return {
                 ['<C-u>'] = { 'scroll_documentation_up', 'scroll_signature_up', 'fallback' },
                 ['<C-d>'] = { 'scroll_documentation_down', 'scroll_signature_down', 'fallback' },
             },
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+                per_filetype = {
+                    gitcommit = { inherit_defaults = true, 'git' },
+                },
+                providers = {
+                    lsp = { async = true },
+                    git = {
+                        name = 'git',
+                        module = 'blink-cmp-git',
+                    },
+                },
+            },
             completion = {
                 ghost_text = {
                     enabled = true,
@@ -28,34 +47,24 @@ return {
                 },
                 list = { selection = { auto_insert = false } },
                 menu = {
+                    auto_show = true,
+                    auto_show_delay_ms = function(_, _) return vim.api.nvim_get_mode().mode == 'c' and 500 or 0 end,
                     winblend = vim.opt.pumblend:get(),
                     draw = {
                         columns = { { 'kind_icon' }, { 'label', gap = 1 } },
                         components = {
                             label = {
-                                text = function(ctx)
-                                    return require('colorful-menu').blink_components_text(ctx)
-                                end,
-                                highlight = function(ctx)
-                                    return require('colorful-menu').blink_components_highlight(ctx)
-                                end,
+                                text = function(ctx) return require('colorful-menu').blink_components_text(ctx) end,
+                                highlight = function(ctx) return require('colorful-menu').blink_components_highlight(ctx) end,
                             },
                             kind_icon = {
                                 text = function(ctx)
-                                    local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
-                                    return kind_icon
+                                    local kind_icon, _, is_default = require('mini.icons').get('lsp', ctx.kind)
+                                    return not is_default and kind_icon or ctx.kind_icon
                                 end,
-                                -- (optional) use highlights from mini.icons
                                 highlight = function(ctx)
-                                    local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
-                                    return hl
-                                end,
-                            },
-                            kind = {
-                                -- (optional) use highlights from mini.icons
-                                highlight = function(ctx)
-                                    local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
-                                    return hl
+                                    local _, hl, is_default = require('mini.icons').get('lsp', ctx.kind)
+                                    return not is_default and hl or ctx.kind_hl
                                 end,
                             },
                         },
@@ -76,21 +85,17 @@ return {
                     winblend = vim.opt.winblend:get(),
                 },
             },
-            sources = {
-                default = function(_)
-                    local success, node = pcall(vim.treesitter.get_node)
-                    if success and node and vim.tbl_contains({ 'comment', 'line_comment', 'block_comment' }, node:type()) then
-                        return { 'buffer' }
-                    elseif vim.bo.filetype == 'lua' then
-                        return { 'lsp', 'path' }
-                    else
-                        return { 'lsp', 'path', 'snippets', 'buffer' }
-                    end
-                end,
-                providers = {
-                    lsp = { async = true },
+            cmdline = {
+                keymap = { preset = 'inherit' },
+                list = {
+                    selection = { preselect = true },
+                },
+                completion = {
+                    menu = { auto_show = true },
+                    ghost_text = { enabled = true },
                 },
             },
+            term = { enabled = true },
             fuzzy = { implementation = 'prefer_rust_with_warning' }
         },
         opts_extend = { 'sources.default' },
