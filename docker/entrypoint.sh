64ab7b266f6302d58a9abfe4e13fa8d5dc5ed199
Chikurin1014 <chikurin1014@gmail.com>
2026-04-17 02:49:47 +0000

    fix(docker): 🐛 fix a bug on docker run

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,12 +3,11 @@
 set -e
 
 if [ -e '~/.initialized' ]; then
-   exit 0 
+   exit 0
 fi
 
-cd ~/dotfiles
 echo 'includes = []' > ~/dotfiles/.dotter/local.toml
 echo 'packages = ["default"]' >> ~/dotfiles/.dotter/local.toml
-~/.local/bin/dotter deploy --force
+cd ~/dotfiles && ~/.local/bin/dotter deploy --force
 
 touch ~/.initialized
