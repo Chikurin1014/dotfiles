d57c78806a008b54c968b47381f07e7a70df8f95
Chikurin1014 <chikurin1014@gmail.com>
2026-07-15 04:29:43 +0000

    feat(fish): ✨ add fish plugins

--- files/config/fish/config.fish
+++ files/config/fish/config.fish
@@ -1,13 +1,13 @@
-set -gx SSH_AUTH_SOCK       "$XDG_RUNTIME_DIR/ssh-agent.socket"
+set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 set -gx FZF_DEFAULT_COMMAND fd
 
-alias ls              "eza --icons --git"
+alias ls "eza --icons --git"
 alias deploy-dotfiles "~/dotfiles/deploy.sh"
 
 function y
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
     command yazi $argv --cwd-file="$tmp"
-    if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+    if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
         builtin cd -- "$cwd"
     end
     command rm -f -- "$tmp"
@@ -20,5 +20,16 @@ if status is-interactive
 
     if command -q nix-your-shell
         nix-your-shell fish | source
+    end
+
+    # install fish plugins
+    if functioins -q fisher
+        curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+        fisher install acomagu/fish-async-prompt
+        fisher install decors/fish-ghq
+        fisher install jorgebucaran/autopair.fish
+        fisher install kidonng/nix.fish
+        fisher install PatrickF1/fzf.fish
     end
 end
